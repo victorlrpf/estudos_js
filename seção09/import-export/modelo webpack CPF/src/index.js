3d190dc785c1f2b5e4d90ec3c3f91b33ca9e5b66
victor <victorlrpf@gmail.com>
2024-01-16 12:34:24 -0300

    completando o exercicio de validador de CPF e iniciando o de senhas

---
+++ seção09/import-export/modelo webpack CPF/src/index.js
@@ -0,0 +1,8 @@
+import GeraCPF from './modules/GeraCPF'
+import './assets/css/style.css'
+
+(function(){
+    const gera = new GeraCPF()
+    const cpfGerado = document.querySelector('.resultado')
+    cpfGerado.innerHTML = gera.geraNovoCpf()
+})()
