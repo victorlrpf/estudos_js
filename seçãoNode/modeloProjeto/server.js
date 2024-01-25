d1749335e44958bd8b118b8fbbacdd412539361d
victor <victorlrpf@gmail.com>
2024-01-25 09:44:13 -0300

    alteração no server

--- seçãoNode/modeloProjeto/server.js
+++ seçãoNode/modeloProjeto/server.js
@@ -1,9 +1,8 @@
 require('dotenv').config()
-
 const express = require('express')
 const app = express()
 
-// Conectando com o cluster
+// Aqui é o mongoose que vai salvar e modelar os dados
 const mongoose = require('mongoose')
 mongoose.connect(process.env.connectionString)
     .then(() => {
@@ -11,21 +10,20 @@ mongoose.connect(process.env.connectionString)
     })
     .catch(e => console.log(e))
 
+//Aqui é apra verrificar o computador do cliente
 const session = require('express-session')
-const MongoStore = require('connect-mongo')
-const flash = require('connect-flash')
+const MongoStore = require('connect-mongo') // para salvar dentro da base de dados
+const flash = require('connect-flash') // mensagens alto destrutivas
 const routes = require('./routes')
 const path = require('path')
 const helmet = require('helmet')
 const csrf = require('csurf')
-const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middlewares')
+const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middlewares') //eles são funções executadas na rota (middlewares)
 
 app.use(helmet())
-
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'public')))
-
 const sessionOptions = session({
     secret: 'OLA',
     store: MongoStore.create({ mongoUrl: process.env.connectionString }),
@@ -36,11 +34,10 @@ const sessionOptions = session({
         httpOnly: true
     }
 })
-
 app.use(sessionOptions)
 app.use(flash())
 
-
+// arquivos para renderizar na tela
 app.set('views', path.resolve(__dirname, 'src', 'views'))
 app.set('view engine', 'ejs')
 app.set('view engine', 'pug')
